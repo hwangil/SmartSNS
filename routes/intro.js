2d7f3e0d675a00276aac498e3c7bff36d05677ea
gilsoo <dolce0629@naver.com>
2017-02-06 13:49:48 +0900

    implement the  regist and login

--- routes/intro.js
+++ routes/intro.js
@@ -9,24 +9,24 @@ module.exports = function(app){
     host      : 'localhost',
     user      : 'root',
     password  : '123147',
-    database  : 'sns_test'
+    database  : 'smart_sns'
   });
   conn.connect();
 
   router.post('/login', function(req, res){
     console.log('# post/login');
-    var query = 'select user_id, user_name, user_gender from user_info where user_id = ? and user_pw = ?';
-    conn.query(query, [req.body.id, req.body.pw], function(err, rows){
+    var query = 'select user_no, user_name, user_gender, user_profile_url from user where user_id = ? and user_pw = ?';
+    conn.query(query, [req.body.user_id, req.body.user_pw], function(err, rows){
       if(err){
         console.log(err);
-        res.status(505).send();
+        res.status(500).send();           // status code 500 : 내부 서버 오류 (여기서 db오류)
       }else{
           if(rows[0]){
             console.log('-> login OK');
             res.status(200).send(JSON.stringify(rows[0]));
           }else{
-            res.status(404).send();
             console.log('-> login failure');
+            res.status(401).send();         // status code 401 : 권한없음
           }
       }
     });
@@ -35,8 +35,12 @@ module.exports = function(app){
 
   router.post('/regist', function(req, res){
     console.log('# post/regist');
-    var query = 'insert into user_info (user_id, user_pw, user_name, user_gender) values (?, ?, ?, ?)';
-    conn.query(query, [req.body.id, req.body.pw, req.body.name, req.body.gender], function(err, rows){
+  
+    var query = 'insert into user (user_id, user_pw, user_name, user_age, user_gender,'+
+    'user_interest_bighash1, user_interest_bighash2, user_interest_bighash3, user_profile_url) values (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    conn.query(query, [req.body.user_id, req.body.user_pw, req.body.user_name, req.body.user_age,
+      req.body.user_gender, req.body.user_interest_bighash1, req.body.user_interest_bighash2, req.body.user_interest_bighash3,
+      req.body.user_profile_url], function(err, rows){
       if(err){
         console.log('-> '+err);
         res.status(505).send();
