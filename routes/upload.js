7d0493996d667e83a400c4dae4648c8d2bcf4cc8
gilsoo <dolce0629@naver.com>
2017-02-15 14:22:50 +0900

    hashtable 설계 및 테스팅 완료

--- routes/upload.js
+++ routes/upload.js
@@ -115,15 +115,7 @@ module.exports = function(app){
                           console.log('increase the bighash count success');
 
                       });
-                      // small hash가 있을때
-                      if(smallhash.length !== 0){
-                        for(var j=0; j<smallhash.length; j++){
-                            insertSmallhashInfo(req, res, bighash, smallhash, i, j);
-                      }
-                    }
-
                       //** smallhash가 없을때 !!
-                      else{
                         var query3 = 'insert into ch_upload (content_no, bighash_no) values('+
                         '(SELECT content_no FROM smart_sns.content WHERE user_no = ? ORDER BY content_no DESC limit 1), ?)';
                         conn.query(query3, [req.body.host, bighash[i]], function(err, rows){
@@ -134,8 +126,14 @@ module.exports = function(app){
                             res.status(200).send();
                           }
                         });
-                      }
                     }
+                    // small hash가 있을때
+                    if(smallhash.length !== 0){
+                      for(var j=0; j<smallhash.length; j++){
+                          insertSmallhashInfo(req, res, smallhash, j);
+
+                    }
+                  }
                   }
                 });
 
@@ -150,9 +148,9 @@ module.exports = function(app){
     });
   });
 
-  var insertSmallhashInfo = function(req, res, bighash, smallhash, big_index, small_index){
-    var query2 = 'select smallhash_no from smallhash where smallhash_name = ? and bighash_no = ?';
-    conn.query(query2, [smallhash[small_index], bighash[big_index]], function(err, rows){
+  var insertSmallhashInfo = function(req, res, smallhash, small_index){
+    var query2 = 'select smallhash_no from smallhash where smallhash_name = ? ';
+    conn.query(query2, [smallhash[small_index]], function(err, rows){
       if(err){
 
       }else{
@@ -162,10 +160,10 @@ module.exports = function(app){
           conn.query('update smallhash set smallhash_count = smallhash_count+1 where smallhash_no = ?', [rows[0].smallhash_no], function(err, rows){
             if(err) console.log(err);
           });
-          //** contents bighash smallhash relation 저장
-          var query3 = 'insert into ch_upload (content_no, bighash_no, smallhash_no) values('+
-          '(SELECT content_no FROM smart_sns.content WHERE user_no = ? ORDER BY content_no DESC limit 1), ?, ?)';
-          conn.query(query3, [req.body.host, bighash[big_index], rows[0].smallhash_no], function(err, rows){
+          //** contents smallhash relation 저장
+          var query3 = 'insert into ch_upload_small (content_no, smallhash_no) values('+
+          '(SELECT content_no FROM smart_sns.content WHERE user_no = ? ORDER BY content_no DESC limit 1), ?)';
+          conn.query(query3, [req.body.host, rows[0].smallhash_no], function(err, rows){
             if(err){
               console.log(err);
             }else{
@@ -176,15 +174,15 @@ module.exports = function(app){
 
         }else{            // 존재하지 않으면 테이블 삽입
           console.log('-> Not exists smallhash ')
-          console.log('->' + smallhash[small_index] +", " +bighash[big_index]);
-          conn.query('insert into smallhash (smallhash_name, bighash_no) values(?, ?)',[smallhash[small_index], bighash[big_index]], function(err, rows){
+          console.log('->' + smallhash[small_index]);
+          conn.query('insert into smallhash (smallhash_name) values(?)',[smallhash[small_index]], function(err, rows){
             if(err) console.log(err);
             else{
               // contents bighash smallhash relation 저장
-              var query3 = 'insert into ch_upload (content_no, bighash_no, smallhash_no) values('+
-              '(SELECT content_no FROM smart_sns.content WHERE user_no = ? ORDER BY content_no DESC limit 1), ?, '+
-              '(SELECT smallhash_no FROM smart_sns.smallhash where smallhash_name = ? and bighash_no = ?))';
-              conn.query(query3, [req.body.host, bighash[big_index], smallhash[small_index], bighash[big_index]], function(err, rows){
+              var query3 = 'insert into ch_upload_small (content_no, smallhash_no) values('+
+              '(SELECT content_no FROM smart_sns.content WHERE user_no = ? ORDER BY content_no DESC limit 1), '+
+              '(SELECT smallhash_no FROM smart_sns.smallhash where smallhash_name = ?))';
+              conn.query(query3, [req.body.host, smallhash[small_index]], function(err, rows){
                 if(err){
                   console.log(err);
                 }else{
@@ -196,71 +194,6 @@ module.exports = function(app){
           });
         }
       }
-    //**  test
-    // console.log('->' + smallhash[j] +", " +bighash[i]);
-    //
-    // conn.query('insert into smallhash (smallhash_name, bighash_no) values(?, ?)',[smallhash[j], bighash[i]], function(err, rows){
-    //   if(err) console.log(err);
-    //
-    // var query3 = 'insert into ch_upload (content_no, bighash_no, smallhash_no) values('+
-    // '(SELECT content_no FROM smart_sns.content WHERE user_no = ? ORDER BY content_no DESC limit 1), ?, '+
-    // '(SELECT smallhash_no FROM smart_sns.smallhash where smallhash_name = ? and bighash_no = ?), ?)';
-    // conn.query(query3, [req.body.host, bighash[i], smallhash[j], bighash[i]], function(err, rows){
-    //   if(err){
-    //     console.log(err);
-    //   }else{
-    //     console.log('-> into relation_file_hash success!');
-    //     res.status(200).send();
-    //   }
-    // });
-    // });
-    // //smallhash 테이블 insert
-    // var query2 = 'select smallhash_no from smallhash where smallhash_name = ? and bighash_no = ?';
-    // conn.query(query2, [smallhash[j], bighash[i]], function(err, rows){
-    //   if(err){
-    //
-    //   }else{
-    //     // if(rows[0]){      // 존재하면
-    //     //   consle.log('-> exists smallhash ');
-    //     //   //** smallhash count 증가
-    //     //   conn.query('update smallhash set smallhash_count = smallhash_count+1 where smallhash_no = ?', [rows[0]], function(err, rows){
-    //     //     if(err) console.log(err);
-    //     //   });
-    //     //   // //** contents bighash smallhash relation 저장
-    //     //   // var query3 = 'insert into ch_upload (content_no, bighash_no, smallhash_no) values('+
-    //     //   // '(SELECT content_no FROM smart_sns.content WHERE user_no = ? ORDER BY content_no DESC limit 1), ?, ?)';
-    //     //   // conn.query(query3, [req.body.host, bighash[i], rows[0]], function(err, rows){
-    //     //   //   if(err){
-    //     //   //     console.log(err);
-    //     //   //   }else{
-    //     //   //     console.log('-> into relation_file_hash success!');
-    //     //   //     res.status(200).send();
-    //     //   //   }
-    //     //   // });
-    //     //
-    //     // }else{            // 존재하지 않으면 테이블 삽입
-    //     //   console.log('-> Not exists smallhash ')
-    //     //   console.log('->' + smallhash[j] +", " +bighash[i]);
-    //     //   conn.query('insert into smallhash (smallhash_name, bighash_no) values(?, ?)',[smallhash[j], bighash[i]], function(err, rows){
-    //     //     if(err) console.log(err);
-    //     //     else{
-    //     //       // contents bighash smallhash relation 저장
-    //     //       var query3 = 'insert into ch_upload (content_no, bighash_no, smallhash_no) values('+
-    //     //       '(SELECT content_no FROM smart_sns.content WHERE user_no = ? ORDER BY content_no DESC limit 1), ?, '+
-    //     //       '(SELECT smallhash_no FROM smart_sns.smallhash where smallhash_name = ? and bighash_no = ?), ?)';
-    //     //       conn.query(query3, [req.body.host, bighash[i], smallhash[j], bighash[i]], function(err, rows){
-    //     //         if(err){
-    //     //           console.log(err);
-    //     //         }else{
-    //     //           console.log('-> into relation_file_hash success!');
-    //     //           res.status(200).send();
-    //     //         }
-    //     //       });
-    //     //     }
-    //     //   });
-    //     // }
-    //   }
-
   });
   };
 
